9e9504aa5622b0e7f15c7c389c5df68e12c543aa
Eddy <eddy.castro@epn.edu.ec>
2025-11-25 10:55:58 -0500

    Entidad Task agregada

--- app/src/main/java/com/grupo/uno/proyecto/data/Task.kt
+++ app/src/main/java/com/grupo/uno/proyecto/data/Task.kt
@@ -1,4 +1,13 @@
 package com.grupo.uno.proyecto.data
 
-class Task {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val description: String,
+    val dueDate: Long, // Guardamos fecha como Timestamp
+    val isCompleted: Boolean = false
+)
--- app/src/main/java/com/grupo/uno/proyecto/data/TaskDao.kt
+++ app/src/main/java/com/grupo/uno/proyecto/data/TaskDao.kt
@@ -1,4 +1,19 @@
 package com.grupo.uno.proyecto.data
 
-class TaskDao {
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM tasks ORDER BY dueDate ASC")
+    fun getAllTasks(): Flow<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(task: Task): Long
+
+    @Delete
+    suspend fun delete(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
 }
